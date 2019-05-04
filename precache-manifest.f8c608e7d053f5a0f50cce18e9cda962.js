dd74937b113fa25ab4b2675ee5f0bb51bd339120
Sekki <bennibrovoldcr@gmail.com>
2019-05-04 10:27:57 +1000

    test version  (0.0.2) fro vkAPP

---
+++ precache-manifest.f8c608e7d053f5a0f50cce18e9cda962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ab9f7f0580b5c51f5bda",
+    "url": "/static/js/main.ab9f7f05.chunk.js"
+  },
+  {
+    "revision": "39dcb6c4f8732925cc80",
+    "url": "/static/js/2.39dcb6c4.chunk.js"
+  },
+  {
+    "revision": "ab9f7f0580b5c51f5bda",
+    "url": "/static/css/main.bf7f4ac2.chunk.css"
+  },
+  {
+    "revision": "39dcb6c4f8732925cc80",
+    "url": "/static/css/2.9130eee1.chunk.css"
+  },
+  {
+    "revision": "7cc1368bbddf878277565866ca3af8a5",
+    "url": "/index.html"
+  }
+];
