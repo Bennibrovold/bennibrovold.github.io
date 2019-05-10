1bd7145c6db279c026306e9edcb934435d4c509f
Sekki <bennibrovoldcr@gmail.com>
2019-05-10 11:51:28 +1000

    VKAPP version(0.0.5)

---
+++ precache-manifest.342cae188544c6c5613d32f580c4586e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0931383ef09282a48b7",
+    "url": "/static/js/main.528e0492.chunk.js"
+  },
+  {
+    "revision": "7610b904220362ac8b47",
+    "url": "/static/js/2.54682c67.chunk.js"
+  },
+  {
+    "revision": "f0931383ef09282a48b7",
+    "url": "/static/css/main.3a621927.chunk.css"
+  },
+  {
+    "revision": "7610b904220362ac8b47",
+    "url": "/static/css/2.9130eee1.chunk.css"
+  },
+  {
+    "revision": "aaff4603cf17ea295fb67dfa59e28de2",
+    "url": "/index.html"
+  }
+];
